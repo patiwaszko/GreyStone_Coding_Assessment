f31e65c09130c96efab4a42fc1c32f3603d5e1a3
patiwaszko <piwaszko19@gmail.com>
2024-02-02 00:55:28 -0500

    Added id field to users to be able to easier identify them

--- main.py
+++ main.py
@@ -10,24 +10,34 @@ class UserCreate(BaseModel):
     username: str
 
 class UserResponse(BaseModel):
+    id: int
     username: str
 
 # Simulated in-memory database
 db = []
+user_id_counter = 1
+existing_usernames = set()
 
 @app.post("/users/", response_model=UserResponse)
 def create_user(user_create: UserCreate):
-    # TODO: DB Read / write
+    global user_id_counter
     
-    existing_user = user_create in db
+    existing_user = user_create.username in existing_usernames
 
     if existing_user:
         raise HTTPException(status_code=400, detail="Username already exists")
+    
+    user_response = UserResponse(id=user_id_counter, username=user_create.username)
 
-    db.append(user_create)
+    user_id_counter += 1
 
-    return user_create
+    db.append(user_response)
+    existing_usernames.add(user_create.username)
+
+    return user_response
 
 @app.get("/users/", response_model=List[UserResponse])
 def list_users():
     return db
+
+
