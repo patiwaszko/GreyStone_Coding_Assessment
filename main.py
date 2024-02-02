138381de10fc205357690f57940977a1b2924d87
patiwaszko <piwaszko19@gmail.com>
2024-02-02 14:45:57 -0500

    Added /loan API endpoint consisting of 2 apis - create_loan and list_loans which allows users to create a loan and also list all created loans.

--- main.py
+++ main.py
@@ -13,9 +13,26 @@ class UserResponse(BaseModel):
     id: int
     username: str
 
+class LoanCreate(BaseModel):
+    amount: float
+    apr: float
+    term: int
+    status: str
+    owner_id: int
+
+class LoanResponse(BaseModel):
+    id: int
+    amount: float
+    apr: float
+    term: int
+    status: str
+    owner_id: int
+
 # Simulated in-memory database
-db = []
+users_db = []
+loans_db = []
 user_id_counter = 1
+loan_id_counter = 1
 existing_usernames = set()
 
 @app.post("/users/", response_model=UserResponse)
@@ -27,17 +44,47 @@ def create_user(user_create: UserCreate):
     if existing_user:
         raise HTTPException(status_code=400, detail="Username already exists")
     
-    user_response = UserResponse(id=user_id_counter, username=user_create.username)
+    user_id = user_id_counter
+    user_response = UserResponse(id=user_id, username=user_create.username)
 
     user_id_counter += 1
 
-    db.append(user_response)
+    users_db.append(user_response)
     existing_usernames.add(user_create.username)
 
     return user_response
 
 @app.get("/users/", response_model=List[UserResponse])
 def list_users():
-    return db
+    return users_db
 
+@app.post("/loans/", response_model=LoanResponse)
+def create_loan(loan_create: LoanCreate):
+    global loan_id_counter
 
+    if loan_create.amount <= 0:
+        raise HTTPException(status_code=400, detail="Loan amount must be greater than zero.")
+
+    if loan_create.apr <= 0:
+        raise HTTPException(status_code=400, detail="Loan interest rate must be greater than zero.")
+
+    if loan_create.term <= 0:
+        raise HTTPException(status_code=400, detail="Loan term must be greater than zero.")
+
+    if loan_create.status.lower() not in ['active', 'inactive']:
+        raise HTTPException(status_code=400, detail="Loan status must be either active or inactive.")
+
+    if loan_create.owner_id > len(users_db) or loan_create.owner_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid owner_id. User does not exist.")
+
+    loan_id = loan_id_counter
+    loan_response = LoanResponse(id=loan_id, **loan_create.model_dump())
+    loans_db.append(loan_response)
+
+    loan_id_counter += 1
+
+    return loan_response
+
+@app.get("/loans/", response_model=List[UserResponse])
+def list_loans():
+    return loans_db
